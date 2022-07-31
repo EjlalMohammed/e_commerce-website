26b918b67266616f1bb67e10ac805c990d9f7062
ejomo <ejlalmohammed3@gmail.com>
2022-07-31 11:00:17 +0300

    add repostery

---
+++ ShoppingCart.Service/Repository/CategoryRepo.cs
@@ -0,0 +1,43 @@
+﻿using shoppingCart.Domain.Models;
+using ShoppingCart.Service.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Service.Repository
+{
+    public class CategoryRepo : ICategory
+    {
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
