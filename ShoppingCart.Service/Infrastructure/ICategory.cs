0d168360b59cbd7b6f836ddefb0fbde08ef21759
ejomo <ejlalmohammed3@gmail.com>
2022-07-31 10:59:18 +0300

    add service lab and interfaces

---
+++ ShoppingCart.Service/Infrastructure/ICategory.cs
@@ -0,0 +1,27 @@
+﻿using shoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ShoppingCart.Service.Infrastructure
+{
+    public interface ICategory
+    {
+        List<Category> GetAllCategories();
+        Category GetCategoryById(int id);
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int id);
+        void save();
+
+
+
+
+    }
+}
+
+
+
+
+
---
+++ ShoppingCart.Service/Infrastructure/IProduct.cs
@@ -0,0 +1,20 @@
+﻿using shoppingCart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Service.Infrastructure
+{
+    public interface IProduct
+    {
+        List<Product> GetAllProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product category);
+        void UpdateProduct(Product category);
+        void DeleteProduct(int id);
+        void save();
+
+    }
+}
