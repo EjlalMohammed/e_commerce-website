cb6bf6615aa03c16cc052abc7af7ca91f1bda3b4
ejomo <ejlalmohammed3@gmail.com>
2022-07-31 10:57:16 +0300

    add domain lab and models'

---
+++ shoppingCart.Domain/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingCart.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public decimal Price { get; set; }
+        public int Code { get; set; }
+
+        public decimal DiscountPercentage { get; set; }
+
+        public string? ProductImage { get; set; }
+
+        public int CreateBy { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public ICollection<ProductsCategory> Categories { get; set; }
+
+
+
+    }
+}
